98e80af1b26fd3c58c347d255a27a5d751bcb350
github username <github email address>
2020-02-14 16:37:08 -0600

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 olivia = User.create(name: "Olivia", email: "olivia@olivia.com", password: "password")
 zach = User.create(name: "Zach", email: "zach@zach.com", password: "password")
 
-Dependent.create(name: "Louis", user_id: olivia.id)
-Dependent.create(name: "Owen", user_id: zach.id)
-Dependent.create(name: "Eva", user_id: olivia.id)
+louis = Dependent.create(name: "Louis", user_id: olivia.id)
+owen = Dependent.create(name: "Owen", user_id: zach.id)
+eva = Dependent.create(name: "Eva", user_id: olivia.id)
+
+hockey = Activity.create(title: "hockey", contact_name: "coach", contact_phone: "coach phone", website: "website", notes: "notes", dependent_id: owen.id)
+soccer = Activity.create(title: "soccer", contact_name: "coach", contact_phone: "coach phone", website: "website", notes: "notes", dependent_id: louis.id)
+lacrosse = Activity.create(title: "lacrosse", contact_name: "coach", contact_phone: "coach phone", website: "website", notes: "notes", dependent_id: eva.id)
