e85c136648e9139c73f4fc61be195380aa201b50
github username <github email address>
2020-02-14 16:12:50 -0600

    fix typo in activities, add missing colon after through

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :dependents
     has_many :user_activities
-    has_many :activities, through :user_activites
+    has_many :activities, through :user_activities
 end
