6943c6ab1305de5453c49ef8412775af2f592ac3
github username <github email address>
2020-02-15 13:40:14 -0600

    create login and show views, create login and users/:id routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < Sinatra::Base
-    
+
+    get '/login' do
+        if current_user
+            redirect '/users/:id'
+        else
+        erb :login
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params["username"])
+        session[:user_id] = @user.id
+        redirect '/users/:id'
+    end
+
+    get '/users/:id' do
+        @user = User.find_by_id(params["id"])
+        erb :'/users/show'
+    end
+
+
 end
