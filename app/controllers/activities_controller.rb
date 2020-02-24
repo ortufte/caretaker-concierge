afd9073d7c0de8500f8039b36fa620a5411e5255
github username <github email address>
2020-02-24 10:14:34 -0600

    add_post_activites_new_route

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,5 +4,20 @@ class ActivitiesController < ApplicationController
         erb :'activities/new'
     end
 
+    post '/activities/new' do
+        @activity = Activity.create(params)
+        @dependent = Dependent.find_by(:name => params[:dependent_name])
 
+        @activity.dependent = @dependent
+        @dependent.activities << @activity
+
+        @user_activity = UserActivity.new 
+        @user_activity.user_id = current_user.id 
+        @user_activity.activity_id = @activity.id
+        @user_activity.save
+
+        redirect "/dependents/#{@activity.dependent.id}"
+    end
+
+    
 end
