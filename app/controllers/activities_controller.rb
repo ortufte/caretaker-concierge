a012396799c9909516cd673a02e1e0a04da3663b
github username <github email address>
2020-02-24 12:31:38 -0600

    create_show_edit_patch_and_delete_routes_in_activities_controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -19,5 +19,48 @@ class ActivitiesController < ApplicationController
         redirect "/dependents/#{@activity.dependent.id}"
     end
 
+    get '/activities/:id' do
+        @activity = Activity.find_by(:id => params[:id])
+        erb :'activities/show'
+    end
+
+    get '/activities/:id/edit' do
+        if !logged_in?
+            redirect '/users/login'
+        end
     
+        @activity = Activity.find_by(:id => params[:id])
+        erb :'activities/edit'
+    end
+    
+    patch '/activities/:id' do
+        @activity = Activity.find_by(:id => params[:id])
+        @user = current_user
+        @user_activity = UserActivity.find_by(:activity_id => params[:id])
+    
+        if @user.id != @user_activity.user_id
+            redirect to "/users/#{@user.id}"
+        else
+            @activity.update(params[:activity])
+            @dependent = @activity.dependent
+            binding.pry
+            redirect to "/dependents/#{@dependent.id}"
+        end
+    end
+    
+    delete '/activities/:id/delete' do
+        if logged_in? 
+            @activity = UserActivities.find_by(:activity_id => params[:id])
+            @user = current_user
+    
+            if @user.id == @activity.user_id
+                @dependent.delete
+                redirect to "/users/#{@user.id}"
+            end
+    
+        else
+            redirect to '/users/login'
+        end
+    end
+
 end
