5d94494969ee455916d63952d1363353ff6fbc00
github username <github email address>
2020-02-24 10:19:10 -0600

    change_redirect_path_in_post_dependents_new_route_and_create_get_route_for_dependents_show_page

--- app/controllers/dependents_controller.rb
+++ app/controllers/dependents_controller.rb
@@ -7,7 +7,12 @@ end
 post '/dependents/new' do
     @dependent = Dependent.create(params)
     @dependent.user_id = current_user.id 
-    redirect '/activities/new'
+    redirect "/dependents/#{@dependent.id}"
+end
+
+get '/dependents/:id' do
+    @dependent = Dependent.find_by(:id => params[:id])
+    erb :'dependents/show'
 end
 
 end
