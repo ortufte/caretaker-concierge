5842259b79dfc057f248078340298a4a0caa3e26
github username <github email address>
2020-02-26 16:20:30 -0600

    add validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,6 @@ class Activity < ActiveRecord::Base
     belongs_to :dependent
     has_many :user_activities
     has_many :users, through: :user_activities
+
+    validates :title, presence: true
 end
