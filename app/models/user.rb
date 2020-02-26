1a833d95d8a28220ea966ffdce33e86136c299da
github username <github email address>
2020-02-26 16:22:50 -0600

    add validation for email uniqueness in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
     has_many :dependents
     has_many :user_activities
     has_many :activities, through: :user_activities
+
+    validates :email, uniqueness: true
 end
