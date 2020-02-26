6d856a4d0f1febfeda3c16943c3f4ea9067ea9ae
github username <github email address>
2020-02-26 10:27:34 -0600

    refactor new, edit, and delete  routes for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,16 +7,10 @@ class ActivitiesController < ApplicationController
     post '/activities/new' do
         @activity = Activity.create(params)
         @dependent = Dependent.find_by(:name => params[:dependent_name])
-
         @activity.dependent = @dependent
         @dependent.activities << @activity
-
-        @user_activity = UserActivity.new 
-        @user_activity.user_id = current_user.id 
-        @user_activity.activity_id = @activity.id
-        @user_activity.save
-
-        redirect "/dependents/#{@activity.dependent.id}"
+        current_user.activities << @activity
+        redirect "/dependents/#{@dependent.id}"
     end
 
     get '/activities/:id' do
@@ -27,40 +21,42 @@ class ActivitiesController < ApplicationController
     get '/activities/:id/edit' do
         if !logged_in?
             redirect '/users/login'
+        else
+            @activity = Activity.find_by(:id => params[:id])
+            erb :'activities/edit'
         end
-    
-        @activity = Activity.find_by(:id => params[:id])
-        erb :'activities/edit'
     end
     
     patch '/activities/:id' do
         @activity = Activity.find_by(:id => params[:id])
-        @user = current_user
-        @user_activity = UserActivity.find_by(:activity_id => params[:id])
-    
-        if @user.id != @user_activity.user_id
-            redirect to "/users/#{@user.id}"
-        else
+        @dependent = @activity.dependent
+        @user_activity = current_user.activities.find_by(params[:id])
+
+        if current_user.dependents.include?(@dependent)
             @activity.update(params[:activity])
-            @dependent = @activity.dependent
-            binding.pry
             redirect to "/dependents/#{@dependent.id}"
+        else
+            redirect to "/users/#{@user.id}"
         end
     end
     
     delete '/activities/:id/delete' do
-        if logged_in? 
-            @activity = UserActivities.find_by(:activity_id => params[:id])
-            @user = current_user
-    
-            if @user.id == @activity.user_id
-                @dependent.delete
+        if !logged_in?
+            redirect '/users/login'
+        else
+            @activity = Activity.find_by(:id => params[:id])
+            @dependent = @activity.dependent
+            @user_activity = current_user.activities.find_by(params[:id])
+
+            if current_user.dependents.include?(@dependent)
+                @activity.delete
+                @user_activity.delete
+                redirect to "/dependents/#{@dependent.id}"
+            else
                 redirect to "/users/#{@user.id}"
             end
-    
-        else
-            redirect to '/users/login'
         end
+
     end
 
 end
